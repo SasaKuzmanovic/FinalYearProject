3bf20dc6841cd8b0b49414a712e97c595080d386
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2022-12-02 14:26:39 +0000

    commands.py added, next step is getting the input working in game

--- app.py
+++ app.py
@@ -1,49 +1,76 @@
 import discord
+import commands
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 
-forward = False
-backwards = False
-left = False
-right = False
-
 myGuild = ""
 role = ""
 
+memberList = []
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}!')
     myGuild = client.get_guild(1047959171825405972)
-    print(f'Guild: {myGuild}')
     role = discord.utils.get(myGuild.roles, id=1047961554680823898)
-    print(f'Role: {role}')
 
+    print(f'We have logged in as {client.user}!')  ## Prints the user name of the Bot when it connects
+    print(f'Guild: {myGuild}')  # Prints the Server name where it is currently operating
+    print(f'Role for sharing gameplay: {role}') # Displays the role that a viewer has to have
 
 @client.event
 async def on_message(message):
     user = message.author
-    memberList = []
-    memberList = discord.Role.members
-    #print(f'User: {user}')
-    #print(user.roles)
-    #if role in user.roles:
-    #print(f'I have the role {role}')    
+
+    forward = False
+    backwards = False
+    left = False
+    right = False   
+
+
+    myGuild = client.get_guild(1047959171825405972)
+    role = discord.utils.get(myGuild.roles, id=1047961554680823898)
+    for member in role.members:
+        memberList.append(member)
+    
     if message.author == client.user:
         return
-    if message.content.startswith('Hello'):
-        await message.channel.send('Sup!')
-    
-    # Control toggling
-    if message.content.startswith('Forward'):
-        forward = True
-        backwards = False
-        await message.channel.send('Forward Control Toggled')
-    if message.content.startswith('Backwards'):
-        backwards = True
-        forward = False
-        message.channel.send('Hello')
-        await message.channel.send('Backwards Control Toggled')
-    #else:
-        #await message.channel.send('You do not have the permission to control the game!')
+
+    if user in memberList:
+        print(f'User: {user} has the role: {role}')
+
+        
+        if message.author == client.user:
+            return
+        if message.content.startswith('Hello'):
+            await message.channel.send('Sup!')
+
+
+        if message.content.startswith('Forward'):
+            commands.forward(forward, backwards)
+            await message.channel.send('Forward Control Toggled!')
+
+
+        # Control toggling
+        #if message.content.startswith('Forward'):
+            #forward = True
+            #if backwards == True:
+                #backwards = False
+                #await message.channel.send('Backwards Control Toggled OFF!')
+            #await message.channel.send('Forward Control Toggled!')
+
+
+        if message.content.startswith('Backwards'):
+            backwards = True
+            if forward == True:
+                forward = False
+                await message.channel.send('Forward Control Toggled OFF!')     
+            await message.channel.send('Backwards Control Toggled!')
+
+    else:
+        await message.channel.send('You do not have the permission to control the game!')
+        
+
 client.run("MTA0Nzk1OTgwMjA4MjUwODg2MA.GrAq9T.Zw3XsSIcGbsvHKuJHHYLoMZYoCOfw-j8LJZNp8")
+
+## Try getting input working in the game
---
+++ commands.py
@@ -0,0 +1,8 @@
+# Viewer playing commands
+
+def forward(t_forward, t_backwards):
+    forward = True
+    backwards = False
+
+#def backwards()
+
